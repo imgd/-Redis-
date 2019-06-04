079beb29e2208dd71ab1f3bce7efe41bf911b670
杨功德 <yanggongde@honasoft.com>
2019-06-04 16:15:16 +0800

    21

---
+++ Test/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    /// <summary>
+    /// Redis 队列简单实现
+    /// </summary>
+    class Program
+    {
+        //同步锁 保证 顺序
+        private static object lockobj = new object();
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"初始化配置...");
+            var helper = new RedisHelper();
+            var key = "test_001";
+            var thredCount = 10;
+            var listCount = 2000000;
+            var dtStart = DateTime.Now;
+
+            helper.AddAsync("test_002", "21321321321",DateTime.Now.AddHours(1));
+
+            Console.WriteLine($"开始写入数据...");
+            for (int i = 1; i <= listCount; i++)
+            {
+                helper.ListRightPush(key, $"{i.ToString()}测试数据XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX大法师空间发挥的空间as富家大室");
+                Console.WriteLine($"写入队列：{i}");
+            }
+
+            //默认执行顺序为 先进先出
+            //当某个数据取出，中间业务执行异常，需要重新把它添加到第一位或优先前面的位置
+            //模拟添加到第一位
+            helper.ListLeftPush(key, "这个是异常插入到第一位");
+
+            Console.WriteLine($"写入完毕，开始拉取队列数据...");
+
+            ManualResetEvent[] _manualEvents = new ManualResetEvent[thredCount];
+
+            for (int i = 0; i < thredCount; i++)
+            {
+                _manualEvents[i] = new ManualResetEvent(false);
+
+                ThreadPool.QueueUserWorkItem((object state) =>
+                {
+                    var sets = state as Imput;
+                    while (true)
+                    {
+                        string item;
+                        lock (lockobj)
+                        {
+                            item = sets.RedisHelper.ListLeftPop(sets.Key);
+                        }
+
+                        if (string.IsNullOrEmpty(item))
+                        {
+                            //标识线程终止
+                            sets.ManualResetEvent.Set();
+                            Console.WriteLine($"线程={sets.Index} ，结束！");
+                            break;
+                        }
+
+                        Console.WriteLine($"线程={sets.Index}  取出队列数据：{item}");
+                    }
+                }, new Imput
+                {
+                    Index = i,
+                    Key = key,
+                    RedisHelper = helper,
+                    ManualResetEvent = _manualEvents[i]
+                });
+            }
+
+            
+
+            WaitHandle.WaitAll(_manualEvents);
+            Console.WriteLine("任务全部结束了");
+            Console.WriteLine($"执行结果：数据={listCount} 分配线程数={thredCount} 执行耗时:{(DateTime.Now - dtStart).TotalSeconds}秒。");
+            Console.ReadLine();
+        }
+
+    }
+
+
+    public class Imput
+    {
+        public int Index { get; set; }
+
+        public string Key { get; set; }
+
+        public RedisHelper RedisHelper { get; set; }
+
+        public ManualResetEvent ManualResetEvent { get; set; }
+    }
+}
---
+++ Test/RedisCache.cs
@@ -0,0 +1,104 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    /// <summary>
+    /// 缓存操作 redis实现
+    /// 建议改用依赖注入来调用
+    /// </summary>
+    public class RedisCache
+    {
+        private string _connectionSettings { get; set; }
+        private IDatabase _db { get; set; }
+
+        public IEnumerable<string> AllKeys => throw new NotImplementedException();
+
+        public Task<IEnumerable<string>> AllKeysAsync => throw new NotImplementedException();
+
+        public RedisCache() { }
+        public RedisCache(string connectionSettings)
+        {
+            _connectionSettings = connectionSettings;
+            _db = new Lazy<IConnectionMultiplexer>(() => ConnectionMultiplexer.Connect(_connectionSettings)).Value.GetDatabase();
+        }
+
+        //list 操作 ：https://blog.csdn.net/nangeali/article/details/81735443
+        //默认不设置过期时间，持久化
+
+        /// <summary>
+        /// add
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="o"></param>
+        /// <param name="expiration"></param>
+        public void AddAsync(string key, string value, DateTimeOffset expiration)
+        {
+            TimeSpan timeSpan = expiration.DateTime.Subtract(DateTime.Now);
+            _db.StringSet(key, value, timeSpan);
+        }
+
+        
+
+        public async Task ListRightPushAsync(string key, string value)
+        {
+            await _db.ListRightPushAsync(key, value);
+        }
+
+        /// <summary>
+        /// 先进先出，在列表的尾部插入元素 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void ListRightPush(string key, string value)
+        {
+            _db.ListRightPush(key, value);
+        }
+
+        /// <summary>
+        /// 先进后出，在列表头部插入元素
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void ListLeftPush(string key, string value)
+        {
+            _db.ListLeftPush(key, value);
+        }
+
+        public async Task<string> ListLeftPopAsync(string key)
+        {
+            return await _db.ListLeftPopAsync(key);
+        }
+
+        /// <summary>
+        /// 左边出栈，获取列表的第一个元素 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string ListLeftPop(string key)
+        {
+            return _db.ListLeftPop(key).ToString();
+        }
+
+        /// <summary>
+        /// 右边出栈，获取列表的最后一个元素  
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string ListRightPop(string key)
+        {
+            return _db.ListRightPop(key).ToString();
+        }
+    }
+
+
+    public class RedisHelper : RedisCache
+    {
+        public RedisHelper() : base("192.168.1.199:26379,password=123456,connectTimeout=5000,allowAdmin=false,defaultDatabase=9")
+        {
+
+        }
+    }
+}
